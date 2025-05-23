34bcfef12d2aef2c11d45ee42df8b36bd2a927f2
CauanValadao <cauanmv1@outlook.com>
2025-05-23 10:35:21 -0300

    heranca

--- exercicioViagem/Evento.java
+++ exercicioViagem/Evento.java
@@ -2,13 +2,27 @@ package exercicioViagem;
 
 public class Evento {
     private String nome;
+    private int duracao;
     
+    public Evento(String nome, int duracao){
+        this.nome = nome;
+        this.duracao = duracao;
+    }
     public Evento(String nome){
         this.nome = nome;
+        this.duracao = 10;
+    }
+    public Evento(){
+        this.nome = "fulano";
+        this.duracao = 10;
     }
 
     public String getNome(){
         return this.nome;
+    }
+
+    public int getDuracao(){
+        return this.duracao;
     }
     
 }
