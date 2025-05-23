582a786ba57512679b00169f21f05224c3655933
Cauan Mendonça Valadão <cauanmv1@outlook.com>
2025-05-23 20:03:49 -0300

    heranca1

--- exercicioViagem/Evento.java
+++ exercicioViagem/Evento.java
@@ -6,9 +6,16 @@ public class Evento {
     public Evento(String nome){
         this.nome = nome;
     }
+    public Evento(){
+        this("evento");
+    }
 
     public String getNome(){
         return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
     }
     
 }
--- exercicioViagem/Pacote.java
+++ exercicioViagem/Pacote.java
@@ -10,11 +10,32 @@ public class Pacote {
         this.eventos = new Evento[quantEventos];
     }
 
+    public Pacote(String nome){
+        this(nome, 30);
+    }
+
+    public Pacote(){
+        this("pacote", 30);
+    }
+
     public void adicionaEvento(String nome){
         if(this.tamanho < this.eventos.length) eventos[this.tamanho++] = new Evento(nome);
     }
 
     public String getNomePacote(){
         return this.nome;
+    }
+
+    public void setNomePacote(String nome){
+        this.nome = nome;
+    }
+
+    public String getNomeEventoX(int i){
+        if(i < tamanho) return eventos[i].getNome();
+        else return "Esse evento nao existe";
+    }
+
+    public void setNomeEventoX(String nome, int i){
+        if(i < tamanho) eventos[i].setNome(nome);
     }
 }
